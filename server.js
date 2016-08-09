26d191f213c0a65840f080fdde3b323c1d44e9d5
Jordan Lumley <Jordan Lumley>
2016-08-08 23:58:42 -0400

    fixing prod issue

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var debug = require('debug'),
     app = require('./app'),
     http = require('http').Server(app);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
-var server = http.listen(app.get('port'), function () {
+var server = http.listen(app.get('port'), '0.0.0.0', function () {
     console.log('Express server listening on port ' + server.address().port);
 });
